3628309a6e75d06c1cdcb3e545870b08c8b1ae86
ThomasF85 <thomas.foeldi@gmail.com>
2022-09-25 20:01:34 +0200

    add demo part 4 code

--- pages/coin/[coin].js
+++ pages/coin/[coin].js
@@ -1,33 +1,23 @@
 import Head from "next/head";
 import Link from "next/link";
-import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
 import Header from "../../components/header/Header";
 import styles from "../../styles/Coin.module.css";
 
-export default function Coin() {
-  const [priceChange, setPriceChange] = useState({});
-  const router = useRouter();
-  const { coin } = router.query;
+export async function getServerSideProps(context) {
+  const { coin } = context.query;
+  const response = await fetch(
+    "https://api.binance.com/api/v3/ticker/24hr?symbol=" +
+      coin.toUpperCase() +
+      "USDT"
+  );
+  const priceChange = await response.json();
 
-  const coinUpperCase = coin ? coin.toUpperCase() : "-";
+  return {
+    props: { priceChange: priceChange, coin: coin.toUpperCase() },
+  };
+}
 
-  async function fetchPriceChange() {
-    if (!router.isReady) {
-      return;
-    }
-    const response = await fetch(
-      `https://api.binance.com/api/v3/ticker/24hr?symbol=${coinUpperCase}USDT`
-    );
-    const priceChange = await response.json();
-
-    setPriceChange(priceChange);
-  }
-
-  useEffect(() => {
-    fetchPriceChange();
-  }, [coin]);
-
+export default function Coin({ priceChange, coin }) {
   const priceChangePercent = priceChange.priceChangePercent
     ? parseFloat(priceChange.priceChangePercent)
     : 0;
@@ -41,10 +31,10 @@ export default function Coin() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>{coinUpperCase}</title>
+        <title>{coin}</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Header title={coinUpperCase} />
+      <Header title={coin} />
       <main className={styles.main}>
         <section className={styles.coinCard}>
           <h2>Price Change 24 hrs</h2>
