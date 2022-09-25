5c39f3ae6132407672cd3d089a9486c75532867b
ThomasF85 <thomas.foeldi@gmail.com>
2022-09-25 14:56:59 +0200

    style portfolio page

--- pages/portfolio.js
+++ pages/portfolio.js
@@ -1,43 +1,9 @@
 import Head from "next/head";
-import { useEffect, useState } from "react";
-import CryptoList from "../components/cryptolist/CryptoList";
 import Header from "../components/header/Header";
 import Navigation from "../components/navigation/Navigation";
-import styles from "../styles/Home.module.css";
+import styles from "../styles/Portfolio.module.css";
 
-export default function Home() {
-  const [cryptos, setCryptos] = useState([]);
-
-  async function fetchPrices() {
-    const response = await fetch("https://api.binance.com/api/v3/ticker/price");
-    const data = await response.json();
-
-    const currentCryptos = data
-      .filter((ticker) => ticker.symbol.endsWith("USDT"))
-      .map((crypto) => ({ ...crypto, symbol: crypto.symbol.slice(0, -4) }));
-
-    setCryptos(
-      currentCryptos.map((crypto) => ({ ...crypto, bookmarked: false }))
-    );
-  }
-
-  useEffect(() => {
-    fetchPrices();
-  }, []);
-
-  function toggleBookmark(symbol) {
-    setCryptos((previousCryptos) =>
-      previousCryptos.map((crypto) =>
-        crypto.symbol === symbol
-          ? {
-              ...crypto,
-              bookmarked: !crypto.bookmarked,
-            }
-          : crypto
-      )
-    );
-  }
-
+export default function Portfolio() {
   return (
     <div className={styles.container}>
       <Head>
